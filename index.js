de85c7792c917e591e53143acfb0cc35eac20a0f
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-10-18 15:52:31 +0700

    tadi salah soal

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-let arr = []
+var roster=[
+["Number","Name","Position","Point Per Game"],
+[12,"Joe Schmo","Center",[14, 32, 7, 0, 23]],
+[9,"Ms. Buckets","Point Guard", [19,0,11,22,0]],
+[31,"Harvey kay","Shooting Guard",[0,30,16,0,25]],
+[7,"Sally Talls","Power Forward", [18,29,26,31,19]],
+[22,"MK DiBoux","Small Forward",[11,0,23,17,0]]
+]
 
-function createObj(name, phase, gender) {
-	arr.push({name : name, phase : phase, gender : gender})
+function arrayToobject(arr){
+
+	let result = []
+	let obj = {}
+	let a = 0
+
+	for(let i=1; i<arr.length; i++){
+		//console.log(arr[i]);
+		obj = {}
+
+		obj[`${arr[0][0]}`] = arr[i][0]
+		obj[`${arr[0][1]}`] = arr[i][1]
+		obj[`${arr[0][2]}`] = arr[i][2]
+		obj[`${arr[0][3]}`] = arr[i][3]
+		result[a]=[]
+		result[a] = obj
+		a++
+	}
+
+	console.log(result);
+	return result
 }
 
-function getData(name) {
-	let result = arr.filter(function( obj ) {
-  		return obj.name == name;
-	})
-
-	return result[0]
-}
-
-createObj('Akbar', 1, 'male')
-createObj('Icha', 1, 'female')
-
-console.log(arr);
-
-console.log(getData('Akbar'));
-console.log(getData('Icha'));
+arrayToobject(roster)
