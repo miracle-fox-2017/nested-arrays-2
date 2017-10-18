ab624f3b1019adf548ebc6ce240092fe36a13827
vbagustinus <vbagustinus@gmail.com>
2017-10-18 17:36:22 +0700

    nested array2

---
+++ index.js
@@ -0,0 +1,26 @@
+let roster =
+[
+  ['Number', 'Name', 'Position', 'Points per Game'],
+  [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23]],
+  [9, 'Mrs. Buckets', 'Point Guard', [19, 32, 11, 22, 0]],
+  [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 23]],
+  [7, 'Sally Talls', 'Power Forward', [18, 29, 26, 31, 19]],
+  [22, 'MK DiBoux', 'Small Forward', [11, 0, 23, 17, 0]],
+]
+
+function convert_roster_format(roster) {
+  var arr = []
+  for (var i = 1; i < roster.length; i++) {  //i=0
+      var obj = {}
+      obj[roster[0][0]] = roster[i][0]
+      obj[roster[0][1]] = roster[i][1]
+      obj[roster[0][2]] = roster[i][2]
+      obj[roster[0][3]] = roster[i][3]
+      arr.push(obj)
+  }
+  return arr
+}
+let object_roster = convert_roster_format(roster)
+console.log(object_roster);
+console.log(object_roster[2]);
+console.log(object_roster[0]['Name']=='Joe Schmo');
