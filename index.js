f73c21838ace4b31ba5b4f9e3fca242e3376d6ab
riskinputra <riski.n.putra@gmail.com>
2017-10-20 19:26:16 +0700

    riskinputra

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-let arr =[]
-function createObject(name, phase, gender) {
-  arr.push({
-    name: name,
-    phase: phase,
-    gender: gender
-  });
-}
-createObject('Akbar', 1, 'male');
-createObject('Icha', 1, 'female');
-createObject('Adit', 2, 'male');
-createObject('Tama', 2, 'male');
-createObject('Rifky', 3, 'male');
+let roster=[
+     ["Number","Name","Position","Points"],
+     [12,"Joe Schmo","Center",[14,32,7,0,23]],
+     [9,"Ms. Buckets","Point Guard",[19,0,11,22,0]],
+     [31,"Harvey Kay","Shooting Guard",[0,30,16,0,25]],
+     [7,"Sally Talls","Power Forward",[18,29,26,31,19]],
+     [22,"MK DiBoux","Small Forward",[11,0,23,17,0]]
+ ];
 
-console.log(arr);
+ function convertRosterFormat(roster){
+     let newArr=[];
+     for (var i = 0; i < roster.length - 1; i++) {
+		     newArr[i] = {};
+    		for (var j = 0; j < roster[0].length; j++) {
+    			newArr[i][roster[0][j]] = roster[i+1][j];
+    		}
+    	}
+     return newArr;
+ }
+
+ console.log(convertRosterFormat(roster));
